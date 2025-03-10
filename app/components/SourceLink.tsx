ef836b16ff59e56cdb0986b85185592c682e698f
Jose-Tejero <j_jtejero@hotmail.com>
2025-03-10 17:40:02 -0600

    Add styles to SourceLink

--- app/components/SourceLink.tsx
+++ app/components/SourceLink.tsx
@@ -1,10 +1,13 @@
+import ExternalLinkIcon from "../icons/ExternalLinkIcon";
+
 export default function SourceLink() {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center w-full max-w-xl mx-auto gap-4 text-gray-500">
       Source
-      <a href="" target="_blank" className="text-blue-500 underline">
+      <a href="" target="_blank" className="text-purple-500 underline">
         Link here
       </a>
+      <ExternalLinkIcon />
     </div>
   );
 }
